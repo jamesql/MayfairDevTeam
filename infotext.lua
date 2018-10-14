5a1c0bb1447a376d65c523b68ea52e53976eeb05
viggo95yoda <43821187+viggo95yoda@users.noreply.github.com>
2018-10-14 17:36:36 +0200

    Update and rename client.lua to infotext.lua

---
+++ infotext.lua
@@ -0,0 +1,8 @@
+Citizen.CreateThread( function()
+    while true do
+        Citizen.Wait(0)
+	local playername = GetPlayerName(source)
+	local playerid = GetPlayerServerId(source)
+	Citizen.InvokeNative(GetHashKey("ADD_TEXT_ENTRY"), 'FE_THDR_GTAO', '~p~ MAYFAIR GAMING - ID: ~w~'..playerid..'~p~ | Name:  ~w~'..playername)
+	end
+end)
